8f419f1fada77d3444cd64079627bdf9241085c2
Han Xiao <han.xiao@zalando.de>
2015-11-30 09:10:29 +0100

    use new homepage

--- update.sh
+++ update.sh
@@ -4,14 +4,11 @@ HOME=/home/han
 . ~/.keychain/`/bin/hostname`-sh
 cd /home/han/Documents/xiaode-backend/
 /usr/bin/java -jar /home/han/Documents/xiaode-backend/target/xiaode.jar --json /home/han/Documents/xiaode-backend/database.json --kw /home/han/Documents/xiaode-backend/keywo$
-cp /home/han/Documents/xiaode-backend/*.json /home/han/Documents/hanxiao.github.io/data/
 cp /home/han/Documents/xiaode-backend/*.lz /home/han/Documents/hanxiao.github.io/data/
 cd /home/han/Documents/hanxiao.github.io/
 scp /home/han/Documents/xiaode-backend/thumbnail/*.jpg  xiaoh@lxhalle.informatik.tu-muenchen.de:/u/halle/xiaoh/home_page/html-data/thumbnail/
-/usr/bin/git add data/database*.json
 /usr/bin/git add data/keywords.json
 /usr/bin/git add data/*.lz
 /usr/bin/git add index.html
 /usr/bin/git commit -m"regular update of database `date`"
 /usr/bin/git push
-
