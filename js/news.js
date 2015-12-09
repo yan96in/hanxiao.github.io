64d16f4fd3b160e46f52d1314f1f08b1f19489eb
Han Xiao <han.xiao@zalando.de>
2015-12-09 07:41:02 +0100

    fix link

--- js/news.js
+++ js/news.js
@@ -4,7 +4,7 @@
 var randomTitles = ["德国2016年经济增速因难民问题下调？",
     "希腊已经开始经济复苏？", "降息？升息？且看欧洲央行和美联储如何做决策",
     "2015年经济回顾，震荡中发展", "欧元人民币汇率今年破7？", "经济报：欧洲的复苏全靠中国",
-    "接受难民是双刃剑还是自刎剑？", "双11消费狂欢背后的世界经济"];
+    "接受难民是双刃剑还是自刎剑？", "双11消费狂欢背后的世界经济", "一场德拉吉和耶伦之间的较量"];
 
 var QueryString = function () {
     // This function is anonymous, is executed immediately and
@@ -66,9 +66,8 @@ function showDetails(param) {
         mainView.router.loadContent(detailtemplate(re[0]));
         var updateNotif = myApp.addNotification({
             title: randomTitles[randInt(randomTitles.length)],
-            subtitle: '下载欧金所app，掌握的欧洲金融脉搏！<div class="download-btn-group"><img src="img/Download_on_the_App_Store_Badge_CN_135x40.svg" height="30px">&nbsp; &nbsp;<img src="img/zh-cn_generic_rgb_wo_60.png" height="30px"></div>',
+            subtitle: '<a href="http://app.ojins.com" onclick=openClick("http://app.ojins.com")>下载欧金所app，掌握的欧洲金融脉搏！<div class="download-btn-group"><img src="img/Download_on_the_App_Store_Badge_CN_135x40.svg" height="30px">&nbsp; &nbsp;<img src="img/zh-cn_generic_rgb_wo_60.png" height="30px"></div></a> ',
             media: '<img src="img/logo-invert-64x64.png" height="40px" style="border-radius: 8px;">',
-            onClick: openClick("http://app.ojins.com")
         });
 
 
@@ -81,8 +80,7 @@ function showDetails(param) {
     } else {
         var updateNotif = myApp.addNotification({
             title: '无法找到该新闻！',
-            media: '<i class="fa fa-chain-broken"></i>',
-            onClick: openClick("http://app.ojins.com")
+            media: '<i class="fa fa-chain-broken"></i>'
         });
     }
 }
